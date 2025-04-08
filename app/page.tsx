3c940bff75be17669beb7aaaeeb4c18faee0838f
Albertechh <luis@bridgepointschool.com.mx>
2025-04-08 12:56:40 -0600

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-<LandingPage />
+export default function PreescolarDashboard() {
+  return (
+    <div>
+      <h1>¡Bienvenido a Preescolar!</h1>
+      <p>Aquí estará nuestro amigo Ingeniero 🐶 enseñando ciudadanía digital.</p>
+    </div>
+  );
+}
